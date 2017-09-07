b7de43af33cdb13913af450b01c33fd2fccd5342
Viewpit <viewpit@hotmail.no>
2017-09-07 22:42:24 +0200

    Add Aurelia-bootstrap components

--- ClientApp/app/components/counter/counter.ts
+++ ClientApp/app/components/counter/counter.ts
@@ -1,5 +1,17 @@
 export class Counter {
     public currentCount = 0;
+    public collapsed = true;
+
+    public position = 'top';
+    public trigger = 'mouseover';
+
+    public toggled(open) {
+        if (open) {
+            console.log('opened');
+        } else {
+            console.log('closed');
+        }
+    }
 
     public incrementCounter() {
         this.currentCount++;
--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -2,6 +2,10 @@ import 'isomorphic-fetch';
 import { Aurelia, PLATFORM } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-fetch-client';
 import 'bootstrap/scss/bootstrap.scss';
+//import { Config } from "aurelia-bootstrapper";
+import * as BootstrapConfig from 'aurelia-bootstrap';
+//import 'bootstrap'
+//import BootstrapConfig from 'aurelia-bootstrap';
 
 declare const IS_DEV_BUILD: boolean; // The value is supplied by Webpack during the build
 
@@ -16,6 +20,9 @@ export function configure(aurelia: Aurelia) {
         const baseUrl = document.getElementsByTagName('base')[0].href;
         config.withBaseUrl(baseUrl);
     });
+
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-bootstrap'));//, (config: BootstrapConfig) => { config.options.version = 4 });
+//        .feature('resources');
 
     aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app/components/app/app')));
 }
